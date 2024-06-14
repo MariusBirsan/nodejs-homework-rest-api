ce34cdb2df6bd83485c93d826f09a27079f9b5e8
Marius Birsan <marius.birsan90@outlook.com>
2024-06-14 18:57:19 +0300

    app.js was updated

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const dbConnection = require("./utils/dbConnection");
+
 const contactsRouter = require("./routes/api/contacts");
-const authRouter = require("./routes/api/auth");
+const usersRouter = require("./routes/api/users");
+
 const app = express();
+
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 dbConnection();
@@ -14,8 +17,9 @@ app.use(express.static("public"));
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/contacts", contactsRouter);
-app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
