c4995b287f9f4851dd87893dc55b372ebd69beed
Marius Birsan <marius.birsan90@outlook.com>
2024-06-14 18:53:51 +0300

    Files were updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,12 +3,16 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user.js");
 require("dotenv").config();
 const gravatar = require("gravatar");
+const { v4: uuidv4 } = require("uuid");
+const sendWithSendGrid = require("../utils/sendEmail.js");
 
 const AuthController = {
   signup,
   login,
   validateAuth,
   getPayloadFromJWT,
+  getUserByValidationToken,
+  updateToken,
 };
 
 const secretForToken = process.env.TOKEN_SECRET;
@@ -36,17 +40,20 @@ async function signup(data) {
 
   const userAvatar = gravatar.url(email);
 
+  const token = uuidv4();
+
   const newUser = new User({
     email: email,
     subscription: "starter",
     token: null,
     avatarURL: userAvatar,
+    verificationToken: token,
+    verify: false,
   });
 
+  sendWithSendGrid(email, token);
   newUser.setPassword(password);
-
   await newUser.save();
-
   return newUser;
 }
 
@@ -57,7 +64,8 @@ async function login(data) {
     throw new Error("Email and password are required");
   }
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email: email, verify: true });
+
   if (!user) {
     throw new Error("Email or password is wrong");
   }
@@ -97,7 +105,7 @@ function validateAuth(req, res, next) {
   console.log("validateAuth middleware called");
   passport.authenticate("jwt", { session: false }, (err, user) => {
     if (!user || err) {
-      console.log("Unauthorized request");
+      console.log("Unauthorized request"); // Log pentru debugging
       return res.status(401).json({
         status: "error",
         code: 401,
@@ -105,10 +113,31 @@ function validateAuth(req, res, next) {
         data: "Unauthorized",
       });
     }
-    console.log("User authenticated:", user);
+    console.log("User authenticated:", user); // Log utilizator autentificat
     req.user = user;
     next();
   })(req, res, next);
+}
+
+async function getUserByValidationToken(token) {
+  const user = await User.findOne({
+    verificationToken: token,
+    verify: false,
+  });
+
+  if (user) {
+    return true;
+  }
+
+  return false;
+}
+
+async function updateToken(email, token) {
+  token = token || uuidv4();
+
+  await User.findOneAndUpdate({ email: email }, { verificationToken: token });
+
+  sendWithSendGrid(email, token);
 }
 
 module.exports = AuthController;
